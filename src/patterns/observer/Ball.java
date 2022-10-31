a1edb67ca04f931ce9d4b380528b749d97f9fece
Kolo <kololueth@yahoo.com>
2022-10-31 02:11:56 -0600

    Add Ball class

---
+++ src/patterns/observer/Ball.java
@@ -0,0 +1,23 @@
+package patterns.observer;
+
+public class Ball implements Subject{
+
+	@Override
+	public void registerObserver(Observer o) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void removeObserver(Observer o) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void notifyObserver() {
+		// TODO Auto-generated method stub
+		
+	}
+
+} // End of class
