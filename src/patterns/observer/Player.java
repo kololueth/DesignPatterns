a40eac95f42683456ab37df085022f51efe51364
Kolo <kololueth@yahoo.com>
2022-10-31 02:12:22 -0600

    Add Player class

---
+++ src/patterns/observer/Player.java
@@ -0,0 +1,11 @@
+package patterns.observer;
+
+public class Player implements Observer{
+
+	@Override
+	public void update() {
+		// TODO Auto-generated method stub
+		
+	} // End of update
+
+} // End of class
